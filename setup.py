ce7cb144336bd61d28296cb591140ecaa5f3c85d
Vasco Pinho <vascogpinho@gmail.com>
2014-08-04 02:23:40 +0100

    markdown => reST

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup
 
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
-
 
 setup(
     name='justcoin-sdk',
-    version='0.0.3',
+    version='0.0.5',
     packages=['justcoin',],
     license='MIT',
     description = "Python SDK for the Justcoin API",
@@ -29,5 +22,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Topic :: Internet",
         ],
-    long_description=read_md('README.md'),
+    long_description=open('README.rst').read(),
 )
